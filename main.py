7a27c9eae01240e72f3299668c4367709af79637
LysssGod_ <141914590+ChinaLysss@users.noreply.github.com>
2024-06-22 21:50:08 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,21 +1,45 @@
 import urllib.request
 import wmi
-def read_text_from_url(url):#定义read_text_from_url函数 通过网络文本获取文本内容
+User_Version = "beta2024.6.22"#填写你的软件版本号 用于下面的验证
+def Read_Version(url):#定义Read_Version函数 获取软件版本
     response = urllib.request.urlopen(url)
     content = response.read().decode('utf-8')#utf-8 也就意味着支持中文
     return content
-url = "https://gitcode.net/bnamc/testreadurl/-/raw/master/Version"#这里改成你的URL
-text_content = read_text_from_url(url)
+Version_URL = "https://gitcode.net/bnamc/testreadurl/-/raw/master/Version"  #这里改成你的URL！！！
+Version = Read_Version(Version_URL)
+def Read_UUID(url):#定义Read_UUIDl函数 从你的验证库中获取UUID
+    response = urllib.request.urlopen(url)
+    content = response.read().decode('utf-8')# utf-8 也就意味着支持中文
+    return content
+UUID_URL = "https://gitcode.net/bnamc/testreadurl/-/raw/master/UUID"  #这里改成你的URL！！！
+UUID = Read_UUID(UUID_URL)
 wmii = wmi.WMI()
-def CPU_USER_NUMBER():#获取CPU硬件
+def CPU_USER_NUMBER(): #获取CPU硬件
     cpu_uuid = ""
     for cpu in wmii.Win32_Processor():
         cpu_uuid += cpu.ProcessorId.strip()
     return cpu_uuid
 user_uuid = CPU_USER_NUMBER()
-if user_uuid in text_content:
-    print("您已通过验证")
+if User_Version in Version:
+    if user_uuid in UUID:
+        print("您已通过验证") #可自行更改
+    else:
+        print("您未通过验证 您的UUID",user_uuid)  #可自行更改
 else:
-    print("您未通过验证 您的UUID为:",user_uuid)
+    print("您不是最新版本 请到官网进行更新！")  #可自行更改
+    print("您的版本号:",User_Version)  #可自行更改
+    print("最新版本",Version)  #可自行更改
 
 #By Lysss 2024.6.15 23:35
+
+"""
+更新日志：
+
+2024.6.22 17:34
+[New]Version检测
+[Fix]UUID换行问题
+[Next]计划在下一次更新中更换UUID算法
+
+2024.6.15 23:35
+[New]UUID验证
+"""
